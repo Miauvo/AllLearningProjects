cb913191d081b79572b7464a9ce7d73c943edcd2
Aglaophot <Aglaophot@gmail.com>
2018-04-07 20:12:16 +0300

    Практика  темы 3- выполнено Тема 3 ДЗ  - выполнено

---
+++ Chapter_003/src/main/java/HW/HW_03_ZD3.java
@@ -0,0 +1,71 @@
+package HW;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HW_03_ZD3 {
+    public HW_03_ZD3() {
+        System.out.println("\n\u001B[34m Задание 3:\u001B[0m");
+        Scanner sc = new Scanner(System.in);
+        int n=0;
+        System.out.println("Введите елементы массива (Например:10,10,10,10)=");
+        String str = sc.nextLine()+",";
+        str = str.replaceAll(" ", ",");
+        str = str.replaceAll(",,", "");
+
+        int[] array = Arrays.stream(str.split(",")).mapToInt(Integer::parseInt).toArray();
+        n=array.length;
+        System.out.println("-----------------");
+        int min=array[0];
+        int max=array[0];
+        int col = 0;
+        for (int i = n-1; i >= 0; i--) {
+            if(min>array[i]){
+                min=array[i];
+            }
+            if(max<array[i]){
+                max=array[i];
+            }
+            if(array[i]==5){
+                col++;
+            }
+            System.out.print("["+array[i]+"]");
+        }
+        System.out.println("\n-----------------");
+
+        System.out.println("-----------------");
+        System.out.println("Минимальное число: "+min);
+        System.out.println("Максимальное число: "+max);
+        System.out.println("Количество повторений числа 5: "+col);
+        Arrays.sort(array);
+        int cols [][] = new int[n+1][2];
+
+        int sum;
+        int index=array.length-1;
+        int znach;
+        int g=0;
+        boolean flag=false;
+        while (true){
+            sum=0;
+            znach=array[index];
+            if (index==0)flag=true;
+            do{
+
+                sum++;
+            }
+            while ((index!=0)&&(array[index]==array[--index]));
+            cols[g][0]= znach;
+            cols[g][1]= sum;
+            g++;
+            if(flag)break;
+        }
+
+        cols = Arrays.copyOfRange(cols,0,g);
+        for (int i = 0; i <cols.length; i++) {
+            System.out.println(Arrays.toString(cols[i]));
+        }
+
+        System.out.println("Отсортированный масив: " + Arrays.toString(array));
+
+    }
+}
--- Chapter_003/src/main/java/Start.java
+++ Chapter_003/src/main/java/Start.java
@@ -1,4 +1,4 @@
-import HW.HW_03_ZD1;
+import HW.HW_03_ZD3;
 
 public class Start {
     final static String LINES_SEPARATOR = "\u001B[32m********************************************************\u001B[0m";
@@ -11,6 +11,8 @@ public class Start {
 //        Practice_03_ZD5 practice_03_zd5 = new Practice_03_ZD5();
 //        Practice_03_ZD6 practice_03_zd6 = new Practice_03_ZD6();
         System.out.println(LINES_SEPARATOR+"\n\u001B[32mДомашнее задание:\n"+LINES_SEPARATOR +"\u001B[0m");
-        HW_03_ZD1 hw_03_zd1= new HW_03_ZD1();
+        //HW_03_ZD1 hw_03_zd1= new HW_03_ZD1();
+        //HW_03_ZD2 hw_03_zd2= new HW_03_ZD2();
+        HW_03_ZD3 hw_03_zd3= new HW_03_ZD3();
     }
 }
